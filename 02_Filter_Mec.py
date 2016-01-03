747b736858f2e15c46d37799500def825aa9c255
paznugit <gplantiv@gmail.com>
2016-01-03 23:15:31 +0100

    Update of script

--- 02_Filter_Mec.py
+++ 02_Filter_Mec.py
@@ -8,6 +8,7 @@ Created on Tue Dec 29 09:27:56 2015
 import pandas as pd
 from random import shuffle
 import pickle
+import numpy as np
 
 csv_input = '../input/dm_mec.csv'
 csv_cible = '../input/dm_mec_ng.csv'
@@ -25,35 +26,41 @@ csv_cible = '../input/dm_mec_ng.csv'
 candidature_threshold = 5
 
 # Loading the dataframe
-dataframe = pd.read_csv(csv_input, sep = '\t')
+dataframe = pd.read_csv(csv_input, sep = '\t', names = ['KC_OFFRE_ID','DN_INDIVIDU_NATIONAL','SCORE'])
 
+print "##################"
+print "Before filtering:"
+print "nb individu: %i" % len(pd.unique(dataframe['DN_INDIVIDU_NATIONAL'].values))
+print "nb offre: %i" % len(pd.unique(dataframe['KC_OFFRE_ID'].values))
 
+# Counting per individual the number of links
+count_mec = dataframe.groupby('DN_INDIVIDU_NATIONAL')['KC_OFFRE_ID'].count().reset_index()
+# Filter to get only the individuals with at least x links
+list_indiv = list(count_mec.loc[count_mec['KC_OFFRE_ID'] >= 5]['DN_INDIVIDU_NATIONAL'])
+dataframe = dataframe.loc[dataframe['DN_INDIVIDU_NATIONAL'].isin(list_indiv)]
 
 # Get the list of individu and produce a conversion index dictionnary
 listIndividu = pd.unique(dataframe['DN_INDIVIDU_NATIONAL'].values)
 shuffle(listIndividu)
-nbindiv = len(listIndividu)
-listIndex = range(nbindiv)
+listIndex = range(len(listIndividu))
 dictIndiv = dict(zip(listIndividu,listIndex))
-print "nb individu: %i" % nbindiv
 
 # Get the list of job offers and produce a conversion index dictionnary
 listOffre = pd.unique(dataframe['KC_OFFRE_ID'].values)
 shuffle(listOffre)
-nbOffre = len(listOffre)
 listIndex = range(len(listOffre))
 dictOffre = dict(zip(listOffre,listIndex))
-print "nb offre: %i" % nbOffre
 
-#print dataframe
 # We convert indexes
 indiv_id = list(dataframe.apply(lambda x:  dictIndiv[x['DN_INDIVIDU_NATIONAL']],axis=1))
-#print indiv_id
 job_id = list(dataframe.apply(lambda x: dictOffre[x['KC_OFFRE_ID']],axis=1))
-#print job_id
-dataframe = dataframe.join(pd.DataFrame(data = {'INDIV_ID': indiv_id, 'JOBOFFER_ID': job_id}))
-dataframe = dataframe.drop(['DN_INDIVIDU_NATIONAL','KC_OFFRE_ID'], axis=1)
+score = list(dataframe['SCORE'])
+print len(job_id)
+print len(np.unique(job_id))
+dataframe = pd.DataFrame(data = {'INDIV_ID': indiv_id, 'JOBOFFER_ID': job_id, 'SCORE': score})
+#dataframe = dataframe.drop(['DN_INDIVIDU_NATIONAL','KC_OFFRE_ID'], axis=1)
 
+# We save the indiv dictionnary
 outputIndiv = open('../input/indiv_dict.pkl', 'wb')
 pickle.dump(dictIndiv, outputIndiv)
 outputIndiv.close()
@@ -63,7 +70,8 @@ with open('../input/indiv_dict.csv', 'w') as outfile:
         outfile.write(",")
         outfile.write(str(value))
         outfile.write("\n")
-        
+   
+# We save the job offer dictionnary     
 outputJob = open('../input/joboffer_dict.pkl', 'wb')
 pickle.dump(dictOffre, outputJob)
 outputJob.close()
@@ -78,13 +86,14 @@ with open('../input/joboffer_dict.csv', 'w') as outfile:
 mydict2 = pickle.load(pkl_file)
 pkl_file.close()'''
 
-# Counting per individual the number of links
-count_mec = dataframe.groupby('INDIV_ID')['JOBOFFER_ID'].count().reset_index()
-# Filter to get only the individuals with at least x links
-list_indiv = list(count_mec.loc[count_mec['JOBOFFER_ID'] >= 5]['INDIV_ID'])
-dataframe = dataframe[dataframe['INDIV_ID'].isin(list_indiv)]
+print len(pd.unique(dataframe['JOBOFFER_ID'].values))
+# Remove the indiv_id-joboffer duplicate of the dataset, since they exist
+dataframe.drop_duplicates(subset=['INDIV_ID','JOBOFFER_ID'], inplace = True)
 
-#dataframe = dataframe[:30]
+print "##################"
+print "After filtering:"
+print "nb individu: %i" % len(pd.unique(dataframe['INDIV_ID'].values))
+print "nb offre: %i" % len(pd.unique(dataframe['JOBOFFER_ID'].values))
 
 # Save to file
 dataframe.to_csv(csv_cible, index=False, float_format='%i')
--- 04_DimensionReduction_SVD.py
+++ 04_DimensionReduction_SVD.py
@@ -8,6 +8,8 @@ Created on Tue Dec 29 10:48:11 2015
 import pandas as pd
 from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import svds
+from scipy import linalg as LA
+import numpy as np
 
 csv_input = '../input/dm_mec_ng_bo.csv'
 
@@ -17,20 +19,52 @@ csv_input = '../input/dm_mec_ng_bo.csv'
 
 dataframe = pd.read_csv(csv_input)
 
-dataframe = dataframe[:50000]
+'''nbIndiv = 80000
+nbOffre = 100000
+listIndiv = range(nbIndiv)
+listOffre = range(nbOffre)
 
-listIndividu = pd.unique(dataframe['INDIV_ID'].values)
-nbindiv = len(listIndividu)
+dataframe = dataframe[dataframe['INDIV_ID'].isin(listIndiv)]
+dataframe = dataframe[dataframe['JOBOFFER_ID'].isin(listOffre)]'''
+
+'''listIndividu = pd.unique(dataframe['INDIV_ID'].values)
+nbIndiv = len(listIndividu)
 
 listOffre = pd.unique(dataframe['JOBOFFER_ID'].values)
-nbOffre = len(listOffre)
+nbOffre = len(listOffre)'''
+
+nbIndiv = len(pd.unique(dataframe['INDIV_ID'].values))
+nbOffre = len(pd.unique(dataframe['JOBOFFER_ID'].values))
+print nbIndiv
+print nbOffre
+
+#print dataframe.loc[dataframe['JOBOFFER_ID'] > 948915]
 
 rows = list(dataframe['INDIV_ID'])
 cols = list(dataframe['JOBOFFER_ID'])
-vals = list(dataframe['SCORE'])
+vals = [float(x) for x in list(dataframe['SCORE'])]
 
-shape = (nbindiv, nbOffre)
+shape = (nbIndiv, nbOffre)
 
 m = coo_matrix((vals, (rows, cols)), shape=shape)
 
-u,s,vt = svds(m,k=15)
+#u,s,vt = svds(m,k=6)
+
+for k in range(300):
+    u,s,vt = svds(m,k=k)
+    print "k=%i" %k
+    print s.shape
+
+#a = (u.dot(np.diag(s))).dot(vt)
+
+
+#print LA.norm(a-m,ord ='fro')
+'''print u.shape
+print s.shape
+print vt.shape
+print np.diag(s)'''
+
+'''print m
+print u
+print s
+print vt'''
